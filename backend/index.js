a80aa74ca016f2a2394540bb02e6f2b1f5019d59
furyprestige <A01769388@itesm.mx>
2026-04-10 00:13:29 -0600

    Added frontend URL in CORS

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://drawroulette.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
